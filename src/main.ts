1005be1a3a26df0b5e4a1452d5e48299883a6b55
jsw4335 <jsw4335@gmail.com>
2026-01-03 01:06:34 +0900

    fix: Prisma DB 연결 시 환경변수 미로드 오류 수정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없는 속성은 자동으로 제거 (보안)
+      forbidNonWhitelisted: true, // DTO에 없는 속성이 들어오면 요청 자체를 차단
+      transform: true, // JSON 데이터를 DTO 클래스 객체로 자동 변환
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,11 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+//아래는 DB접근이 안되어 제가 따로 추가했습니다.
+import * as dotenv from 'dotenv';
+
+// 1. 파일 최상단에서 환경 변수를 명시적으로 로드합니다.
+dotenv.config();
 
 @Injectable()
 export class PrismaService
@@ -8,6 +13,8 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
+    console.log('로드된 DB URL:', process.env.DATABASE_URL);
+
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL!,
     });

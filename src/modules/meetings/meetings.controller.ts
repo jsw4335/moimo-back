2fd53a8034699543a64044704cf6c80cc9322f48
jsw4335 <jsw4335@gmail.com>
2026-01-06 02:05:13 +0900

    배포테스트용 hostID수정

--- src/modules/meetings/meetings.controller.ts
+++ src/modules/meetings/meetings.controller.ts
@@ -23,7 +23,7 @@ export class MeetingsController {
     // 임시로 ID가 1인 유저가 방장이라고 가정하고 전달합니다.
     // (추후 JWT 가드 설치 후 req.user.id로 교체될 예정입니다)
     try {
-      const tempHostId = 1;
+      const tempHostId = 2;
       await this.meetingsService.create(dto, tempHostId);
 
       return res.status(HttpStatus.CREATED).send();

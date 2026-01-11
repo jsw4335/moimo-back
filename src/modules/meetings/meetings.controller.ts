ec74656fbecf6b1c347f86f29695d7a842d51d8f
JO SEONG WON <96003896+jsw4335@users.noreply.github.com>
2026-01-12 02:44:59 +0900

    [refactor]주석제거
    [refactor]주석제거

--- src/modules/meetings/meetings.controller.ts
+++ src/modules/meetings/meetings.controller.ts
@@ -28,8 +28,6 @@ export class MeetingsController {
     @Res() res: express.Response,
     @Req() req: express.Request & { user: JwtPayload },
   ) {
-    // 임시로 ID가 1인 유저가 방장이라고 가정하고 전달합니다.
-    // (추후 JWT 가드 설치 후 req.user.id로 교체될 예정입니다)
     try {
       const hostId = req.user.id;
       await this.meetingsService.create(dto, hostId);

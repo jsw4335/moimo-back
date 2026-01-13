78bbeaf6e09d393ecb4f27eb28b707255bee3b96
jsw4335 <jsw4335@gmail.com>
2026-01-13 22:05:56 +0900

    [fix] 프론트와 협의 후 수정한 최종 스키마

--- prisma/migrations/20260105161358_init_neon_db/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `nickname` on table `users` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "nickname" SET NOT NULL,
-ALTER COLUMN "resetCode" DROP NOT NULL;
--- prisma/migrations/20260107062259_init_supabase/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropIndex
-DROP INDEX "users_resetCode_key";
-
--- AlterTable
-ALTER TABLE "meetings" ADD COLUMN     "image" TEXT;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "image" TEXT,
-ALTER COLUMN "updated_at" DROP DEFAULT;
